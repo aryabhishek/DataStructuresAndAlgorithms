c9f76916af9c386c0f990bd8db899062213cfbde
Aryabhishek Verma <48897634+aryabhishek@users.noreply.github.com>
2025-02-03 14:59:51 +0530

    done

--- BinarySearch/BsOn1dArrays/countNegativeNumbersInASortedMatrix.py
+++ BinarySearch/BsOn1dArrays/countNegativeNumbersInASortedMatrix.py
@@ -1,5 +1,7 @@
 """
+Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.
 
+https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix
 """
 
 
