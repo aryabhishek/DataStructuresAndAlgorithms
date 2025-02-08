e50d8bac446b800c32e12d4db7e87066039e8ba0
Aryabhishek Verma <48897634+aryabhishek@users.noreply.github.com>
2025-02-08 12:57:07 +0530

    modified sqrt(x)

--- TopInterview150/Easy/sqrt(x).py
+++ TopInterview150/Easy/sqrt(x).py
@@ -5,18 +5,19 @@ For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
 Link: https://leetcode.com/problems/sqrtx/description/?envType=study-plan-v2&envId=top-interview-150
 """
 
+from math import ceil
+
 
 class Solution:
     def mySqrt(self, x: int) -> int:
-        l = 1
-        r = x
+        l, r = 0, ceil(x / 2)
 
-        while True:
-            mid = l + (r - l) // 2
+        while l <= r:
+            m = l + (r - l) // 2
 
-            if mid**2 < x:
-                l = mid
-            elif mid**2 > x:
-                r = mid
-            if mid**2 <= x < (mid + 1) ** 2:
-                return mid
+            if m * m <= x < (m + 1) * (m + 1):
+                return m
+            if m * m < x:
+                l = m + 1
+            elif m * m > x:
+                r = m - 1
